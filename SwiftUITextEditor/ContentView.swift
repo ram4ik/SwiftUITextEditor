b1e9ebecb29166a4651f7837c16e6d9baf23948f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 07:17:41 +0300

    init

--- SwiftUITextEditor/ContentView.swift
+++ SwiftUITextEditor/ContentView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            List(0 ..< 5) { item in
+                NavigationLink(
+                    destination: NotesView(),
+                    label: {
+                        /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                    })
+            }.navigationTitle("Notes App")
+        }
+    }
+}
+
+struct NotesView: View {
+    @State private var text = "Edit this text!"
+    
+    var body: some View {
+        TextEditor(text: $text)
+            .navigationBarTitle("", displayMode: .inline)
     }
 }
 
